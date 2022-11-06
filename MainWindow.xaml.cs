79607614d2e67721aa88d258f7529d5007a36c18
sergey <sergey>
2022-11-06 17:58:28 +0300

    add trand

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,23 +2,12 @@
 using System;
 using System.Runtime.CompilerServices;
 using System.Security.AccessControl;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
 using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using ARMA_MODBUS_HMI.Properties;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Navigation;
-//using System.Windows.Shapes;
-//using System.Windows.Threading;
-//using System.Reflection.Emit;
+using ScottPlot;
+using System.Linq;
 
 namespace ARMA_MODBUS_HMI
 {
@@ -33,11 +22,43 @@ namespace ARMA_MODBUS_HMI
         int[] readHoldingRegisters;
 
         
- 
+
+        double[] level_trand = new double[190];
+        double[] rpm_trand = new double[190];
 
         public MainWindow()
         {
             InitializeComponent();
+            WpfPlot1.Plot.Style(System.Drawing.Color.Transparent);
+            WpfPlot1.BorderBrush = new SolidColorBrush(Colors.Black);
+            WpfPlot1.Plot.SetAxisLimits(yMin: 0, yMax: 100, xMin:0, xMax: 100);
+            
+            WpfPlot1.Plot.Title("");
+            WpfPlot1.Plot.XLabel(null);
+            WpfPlot1.Plot.YLabel(null);
+            WpfPlot1.Plot.Legend(true, location: ScottPlot.Alignment.UpperLeft);
+            WpfPlot1.Plot.Frameless(true);
+            WpfPlot1.Plot.Style(ScottPlot.Style.Seaborn);
+  
+
+
+            // plt.Grid(false);
+
+            var signalLevel =  WpfPlot1.Plot.AddSignal(level_trand, sampleRate: 2,  label: "LVL", color: System.Drawing.Color.Green);
+            // WpfPlot1.Plot.AddSignal(level_trand, label: "LVL",sampleRate: 5,color: System.Drawing.Color.Green);
+            signalLevel.Label = "LVL";
+            signalLevel.Color = System.Drawing.Color.Green;
+            signalLevel.MarkerSize = 4;
+            signalLevel.LineWidth = 2;
+
+            var signalRPM = WpfPlot1.Plot.AddSignal(rpm_trand, sampleRate: 2, label: "RPM", color: System.Drawing.Color.Black);
+            // WpfPlot1.Plot.AddSignal(rpm_trand,label:"RPM",  sampleRate: 5, color: System.Drawing.Color.Black);
+            signalRPM.Label = "RPM";
+            signalRPM.Color = System.Drawing.Color.Black;
+            signalRPM.MarkerSize = 4;
+            signalRPM.LineWidth = 2;
+            WpfPlot1.Refresh();
+
 
         }
 
@@ -65,7 +86,17 @@ namespace ARMA_MODBUS_HMI
                     {
                         txb_SetPoint.Text = readHoldingRegisters[0].ToString();
                     }
-               
+
+
+
+                    // trand
+                    Array.Copy(level_trand, 1, level_trand, 0, level_trand.Length - 1);
+                    Array.Copy(rpm_trand, 1, rpm_trand, 0, rpm_trand.Length - 1);
+                    level_trand[level_trand.Length - 1] = readHoldingRegisters[1];
+                    rpm_trand[rpm_trand.Length - 1] = readHoldingRegisters[5] * 100 / 2400;
+                 //   WpfPlot1.Plot.SetAxisLimits(yMin: 0, yMax: level_trand.Max() * 1.2);
+                  
+
                 }
                 catch
                 {
@@ -88,6 +119,7 @@ namespace ARMA_MODBUS_HMI
            
                // connect();
             }
+            WpfPlot1.Refresh();
             dispatcherTimer.Start();
         }
 
